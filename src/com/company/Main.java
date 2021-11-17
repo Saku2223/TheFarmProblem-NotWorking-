77624c81f1ceacac75918ca28b04a5d110f80d76
Matei Tenche <MT210673@reigate.ac.uk>
2021-11-17 09:06:53 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static int animals(int chickens, int cows, int pigs){
+        return ((chickens * 2)+(cows * 2)+(pigs * 2));
+    }
+}
